9f5f59c5068d46d1987e0e0783dcc9a5387fbbeb
Hagen Stanek <stanek@roblet.org>
2021-03-16 03:18:43 +0100

    Doc wording for Robot corrected + stronger reference to unit terminology

--- root/src/main/java/org/roblet/Robot.java
+++ root/src/main/java/org/roblet/Robot.java
@@ -29,29 +29,40 @@ public interface  Robot
 {
 
     /**
-     * Return the implemenation of some {@link Unit} definition as available
-     * in the underlying roblet server.
+     * Return an instance of an implementation of some {@link Unit} definition
+     * as available in the underlying roblet server.
+
+<BLOCKQUOTE>
+    Refer to {@link Unit} regarding the terminology used.
+</BLOCKQUOTE>
 
 <P>
     Although unit definitions are interfaces they nevertheless result in classes
     to be provided here.
-    So in case the implementation of some {@code MyUnit} interface is looked
+    So in case the instance of an implementation
+    of some {@code MyUnit} interface is looked
     for then the parameter would be {@code MyUnit.class}.
 </P>
+<BLOCKQUOTE><PRE>
+MyUnit  myUnit = (MyUnit) robot. getUnit (MyUnit.class);
+if (myUnit == null)
+    // ...
+// ...
+</PRE></BLOCKQUOTE>
 
 <P>
     The result can be {@code null} for the case that the roblet server cannot
-    provide the implementation.
+    provide an instance.
     This should be tested or at least taken into account.
 </P>
 
 <P>
     There is no guarantee that two subsequent calls with the same
-    unit definition will result in the same unit implementation being returned.
+    unit definition will result in the same unit instance being returned.
 </P>
 
-     * @param  definition  class of the unit interface definition
-     * @return  unit implementation or {@code null} if unavailable
+     * @param  definition  class of the unit definition
+     * @return  unit instance or {@code null} if unavailable
      * @see  Roblet
      */
     public Unit  getUnit (Class definition);
