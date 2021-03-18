a29857a0acb9f8ca9f211d11b1caf66687a4d4c9
Hagen Stanek <stanek@roblet.org>
2021-03-18 07:44:04 +0100

    Turn into a module

---
+++ root/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: LGPL-2.1
+// (C) Copyright Hagen Stanek. All rights reserved.
+
+/**
+ * This module contains definitions essential to the roblet world.
+ * @author Hagen Stanek
+ */
+module  roblet.root
+{
+    exports org.roblet;
+}
